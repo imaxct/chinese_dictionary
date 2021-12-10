1cb5a8c4bab5076874e7b71de1abfffa959bb70e
Hao Dong <donghao@microsoft.com>
2021-12-10 18:13:37 +0800

    Open DB readonly

--- lib/common/db_helper.dart
+++ lib/common/db_helper.dart
@@ -29,6 +29,6 @@ class DbHelper {
     var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
     await File(path).writeAsBytes(bytes, flush: true);
 
-    return await openDatabase(path);
+    return await openDatabase(path, readOnly: true);
   }
 }
